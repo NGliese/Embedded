dae3b58d7544d43c81501192d8600163bd65a95e
Nikolaj <nikolajgliese@hotmail.com>
2022-01-29 09:35:15 +0100

    readme update

--- frontend/test2/setupJest.js
+++
@@ -1,3 +0,0 @@
-//setupJest.js or similar file
-require('jest-fetch-mock').enableMocks();
-global.Promise = jest.requireActual('promise');
--- frontend/test2/src/OperatorsInterface/Routes/OperatorPanel.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Styling/Navbar.css";
-//import OperatorNavbar from "./OperatorNavbar";
-
-function OperatorPanel() {
-  return <div className="main">operators&apos; panel</div>;
-}
-
-export default OperatorPanel;
